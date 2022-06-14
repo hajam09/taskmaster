f1097d4595bb4e0028ca63ac1f2d390f49e78bb8
DESKTOP-9M5JTD4\HM <hajam09@yahoo.com>
2022-06-14 20:50:39 +0100

    bakerOperations to create model instances

--- accounts/models.py
+++ accounts/models.py
@@ -94,6 +94,7 @@ class Team(BaseModel):
 class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     url = models.SlugField(max_length=10, editable=settings.DEBUG, unique=True, default=generateString, db_index=True)
+    publicName = models.CharField(max_length=2048, blank=True, null=True)
     jobTitle = models.CharField(max_length=2048, blank=True, null=True)
     department = models.CharField(max_length=2048, blank=True, null=True)
     profilePicture = models.ImageField(upload_to='profile-picture', blank=True, null=True, default=getRandomAvatar)
---
+++ taskmaster/operations/bakerOperations.py
@@ -0,0 +1,58 @@
+import random
+
+from django.contrib.auth.models import User
+from django.db import connection
+from faker import Faker
+
+from accounts.models import Profile
+
+all_tables = connection.introspection.table_names()
+
+# CONSTANT VALUES
+EMAIL_DOMAINS = ["@yahoo", "@gmail", "@outlook", "@hotmail"]
+DOMAINS = [".co.uk", ".com", ".co.in", ".net", ".us"]
+BOOLEAN = [True, False]
+JOB_TITLE = ['Software Engineer', 'Project Owner', 'Project Manager', 'UX/UI Designer', 'Solutions Architect']
+
+
+def createUserObjects(limit=20, maxLimit=20):
+    currentCount = User.objects.count()
+    remaining = maxLimit - currentCount
+
+    if limit == 0 or currentCount > maxLimit:
+        return User.objects.all()[:limit]
+
+    BULK_USERS = []
+    uniqueEmails = []
+
+    for _ in range(remaining):
+        fake = Faker()
+        firstName = fake.unique.first_name()
+        lastName = fake.unique.last_name()
+        email = firstName.lower() + '.' + lastName.lower() + random.choice(EMAIL_DOMAINS) + random.choice(DOMAINS)
+        password = 'RanDomPasWord56'
+
+        BULK_USERS.append(
+            User(username=email, email=email, password=password, first_name=firstName, last_name=lastName)
+        )
+        uniqueEmails.append(email)
+
+    User.objects.bulk_create(BULK_USERS)
+    return User.objects.filter(email__in=uniqueEmails)
+
+
+def createProfiles(users=None):
+    if users is None or len(users) == 0:
+        users = createUserObjects()
+
+    Profile.objects.bulk_create(
+        [
+            Profile(
+                user=user,
+                publicName=user.get_full_name(),
+                jobTitle=random.choice(JOB_TITLE),
+            )
+        ]
+        for user in users
+    )
+    return Profile.objects.filter(user__in=users)
